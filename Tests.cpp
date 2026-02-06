7c7aee3335d9d9831e968803f81cd375cc03d9cb
Francis153 <lolfriky153@gmail.com>
2026-02-06 12:00:15 +0300

    Update Tests.cpp

--- Tests.cpp
+++ Tests.cpp
@@ -1,1 +1,15 @@
 
+#include "Library.h"
+
+int main() {
+    Library lib;
+
+    lib.addBook(Book(10, "Test Book", "Tester"));
+    lib.addUser(User(1, "Test User"));
+
+    lib.borrowBook(1, 10);
+    lib.returnBook(1, 10);
+    lib.borrowBook(1, 99); // negative
+
+    return 0;
+}
