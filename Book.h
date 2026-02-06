6cfb08076f7a681f26da5c3f34bd53eeedf0d616
Francis153 <lolfriky153@gmail.com>
2026-02-06 11:27:10 +0300

    Update Book.h

--- Book.h
+++ Book.h
@@ -1,1 +1,39 @@
+#ifndef BOOK_H
+#define BOOK_H
 
+#include <iostream>
+using namespace std;
+
+class Book {
+private:
+    int id;
+    string title;
+    string author;
+    bool isAvailable;
+
+public:
+    Book(int id, string title, string author) {
+        this->id = id;
+        this->title = title;
+        this->author = author;
+        isAvailable = true;
+    }
+
+    int getId() { return id; }
+    string getTitle() { return title; }
+    string getAuthor() { return author; }
+    bool available() { return isAvailable; }
+
+    void borrowBook() { isAvailable = false; }
+    void returnBook() { isAvailable = true; }
+
+    void display() {
+        cout << "ID: " << id
+             << ", Title: " << title
+             << ", Author: " << author
+             << ", Available: " << (isAvailable ? "Yes" : "No")
+             << endl;
+    }
+};
+
+#endif
