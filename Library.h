67233807cda2b961b3fd6f5c494b8e0565597982
Francis153 <lolfriky153@gmail.com>
2026-02-06 13:14:45 +0300

    Update Library.h

--- Library.h
+++ Library.h
@@ -1,95 +1,77 @@
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+#include "Book.h"
+#include "User.h"
 #include <vector>
+using namespace std;
 
 class Library {
 private:
-    vector<Book> books;
-    vector<User> users;
+    vector<Book> allBooks;
+    vector<User> allUsers;
 
-    void removeBook(int id)
-   {
-        for (int i = 0; i < books.size(); i++) 
-        {
-        if (books[i].getId() == id)
-        {
-            books.erase(books.begin() + i);
-            cout << "Book removed.\n";
-        return;
-        }
-        }
-        cout << "Book not found.\n";
-    }
+public:
+    void addBook(Book b){ allBooks.push_back(b); }
+    void addUser(User u){ allUsers.push_back(u); }
 
-    void searchByTitle(string title)
-   {
-        for (Book &b : books) 
-        {
-            if (b.getTitle() == title) 
-        {
-                b.display();
+    void removeBook(int bookNum){
+        for(int i=0;i<allBooks.size(); i++){
+            if(allBooks[i].getNumber()==bookNum){
+                allBooks.erase(allBooks.begin()+i);
+                cout << "Book removed!\n";
                 return;
         }
         }
-        cout << "Book not found.\n";
+        cout << "Book not found!\n";
     }
 
-    void addUser(User u)
-{
-        users.push_back(u);
-    }
-
-    void borrowBook(int userId, int bookId)
-{
-        for (Book &b : books) 
-        {
-        if (b.getId() == bookId && b.available()) 
-        {
-                b.borrowBook();
-        for (User &u : users)
-        {
-        if (u.getId() == userId)
-        {
-                u.borrow(bookId);
-        cout << "Book borrowed successfully.\n";
-        return;
+    void searchBook(string name){
+        for(int i=0;i<allBooks.size(); i++){
+            if(allBooks[i].getName()==name){
+                allBooks[i].showBook();
+                return;
         }
         }
-            }
+        cout << "Book not found!\n";
+    }
+
+    void borrowBook(int userId, int bookId){
+        for(int i=0;i<allBooks.size(); i++){
+         if(allBooks[i].getNumber()==bookId && allBooks[i].available()){
+                allBooks[i].borrowBook();
+        for(int j=0;j<allUsers.size(); j++){
+        if(allUsers[j].getId()==userId){
+                allUsers[j].borrowBook(bookId);
+                cout << "Book borrowed!\n";
+       return
         }
-        cout << "Borrow failed.\n";
-   
-
-public:
-    void addBook(Book b)
-    {
-        books.push_back(b);
-    }
-    }
-
-    void returnBook(int userId, int bookId) 
-{
-        for (Book &b : books)
-            {
-            if (b.getId() == bookId)
-            {
-                b.returnBook();
-                for (User &u : users)
-            {
-                    if (u.getId() == userId)
-            {
-                        u.returnBook(bookId);
-                        cout << "Book returned.\n";
-                        return;
-            }
-            }
-      }
         }
+        }
+        }
+        cout << "Cannot borrow book.\n";
     }
 
-    void displayBooks()
+    void returnBook(int userId, int bookId){
+       for(int i=0;i<allBooks.size(); i++){
+       if(allBooks[i].getNumber()==bookId){
+                allBooks[i].returnBook();
+       for(int j=0;j<allUsers.size(); j++
+        if(allUsers[j].getId()==userId){
+                        allUsers[j].returnBook(bookId);
+                        cout << "Book returned!\n";
+           return;
+         }
+         }
+         }
+         }
+    }
+
+    void showAllBooks() 
    {
-        for (Book &b : books)
-            b.display();
+        for(int i=0;i<allBooks.size(); i++){
+            allBooks[i].showBook(); 
+    }
     }
 };
-
 #endif
