143d1a8e37a0b81620452bf92a832823d1ee2506
Francis153 <lolfriky153@gmail.com>
2026-02-06 12:40:13 +0300

    Refactor main function and update book/user data

--- Main.cpp
+++ Main.cpp
@@ -1,32 +1,29 @@
 #include "Library.h"
 
-int main() {
+int main() 
+{
     Library lib;
 
-    lib.addBook(Book(1, "C++ Basics", "Bjarne"));
-    lib.addBook(Book(2, "OOP Concepts", "James"));
-
-    lib.addUser(User(101, "Alice"));
+    lib.addBook(Book(3, "C++ Basics", "Kamau"));
+    lib.addBook(Book(6, "OOP Concepts", "Celly"));
+    
+  lib.addUser(User(101, "Alice"));
     lib.addUser(User(102, "Bob"));
-
     cout << "\nAll Books:\n";
     lib.displayBooks();
-
     cout << "\nSearch Test:\n";
-    lib.searchByTitle("C++ Basics");
-    lib.searchByTitle("Java");
-
+    lib.searchByTitle("Physics");
+    
+    lib.searchByTitle("English");
     cout << "\nBorrow Test:\n";
-    lib.borrowBook(101, 1);
-    lib.borrowBook(102, 1);
-
+    
+    lib.borrowBook(101, 3);
+    lib.borrowBook(102, 3);
     cout << "\nReturn Test:\n";
-    lib.returnBook(101, 1);
-    lib.borrowBook(102, 1);
-
-    cout << "\nRemove Test:\n";
-    lib.removeBook(2);
+    lib.returnBook(101, 3);
+    lib.borrowBook(102, 3);
+  cout << "\nRemove Test:\n";
+    lib.removeBook(6);
     lib.removeBook(99);
-
     return 0;
 }
